5c08d15906dc1a49382379f6210466f76d48ba6e
Sasson Havusha <sassonh@verizon.net>
2024-08-04 13:15:32 -0400

    rewrote app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom/client';
+import ContextManager from './utils/ContextManager';
+import FamilyTreeView from './components/familyTree/FamilyTreeView';
+import GEDFileGetter from './components/familyTree/GEDFileGetter';
 
-function App() {
-  return (
-    <div className="App">
-     Family Tree Viewer
-    </div>
-  );
-}
+  function App() {
+    const params = ContextManager.getInstance().getContext();
+    const keyStore = localStorage.getItem('keyStore');
 
-export default App;
+    if (keyStore) {
+      localStorage.setItem('keyStore', '');
+      const ctx = JSON.parse(localStorage.getItem(keyStore) as string);
+      ContextManager.getInstance().setContext(ctx);
+      if (ctx.onSelect) ctx.onSelect();
+    } 
+
+    return (
+      <>
+         <GEDFileGetter { ...params}/>
+         keyStore && <GEDFileGetter { ...params}/>
+         !keyStore && <GEDFileGetter { ...params}/>
+      </>
+    )
+  }
+
+
+export default App
