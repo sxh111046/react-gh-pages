cf1504de62b210e23d1115d2ea2b9eb578d0abfa
Sasson Havusha <sassonh@verizon.net>
2024-08-08 16:15:33 -0400

    fixed issue

--- src/utils/TreeBuilder.ts
+++ src/utils/TreeBuilder.ts
@@ -9,21 +9,20 @@ class TreeBuilder {
     public build() {
         try{
             const context =  ContextManager.getInstance().getContext();
-            if (!context.initialized) {
-                const parser: FamilyTreeParser = new FamilyTreeParser(context.fileContent as string);
-                parser.load();
-                if (parser.getMembers().size === 0) {
-                    new ErrorHandler("this family tree does not have members");
-                }
-                const treeIndex = new TreeIndex(parser.getMembers());
-                treeIndex.initialize();
-                context.familyIndex = treeIndex.getFamilyIndex();
-                context.personIndex = treeIndex.getPersonIndex();
-                context.treeRoots = treeIndex.buildTrees();
-                context.selectedPerson = treeIndex.getPerson(context.root as string);
-                context.subtreeRoot = treeIndex.getPerson(context.root as string);
+            const parser: FamilyTreeParser = new FamilyTreeParser(context.fileContent as string);
+            parser.load();
+            if (parser.getMembers().size === 0) {
                 context.initialized = true;
+                new ErrorHandler("this family tree does not have members");
             }
+            const treeIndex = new TreeIndex(parser.getMembers());
+            treeIndex.initialize();
+            context.familyIndex = treeIndex.getFamilyIndex();
+            context.personIndex = treeIndex.getPersonIndex();
+            context.treeRoots = treeIndex.buildTrees();
+            context.selectedPerson = treeIndex.getPerson(context.root as string);
+            context.subtreeRoot = treeIndex.getPerson(context.root as string);
+            context.initialized = true;
         }
         catch(ex) {
             new ErrorHandler('unexpected error occured during tree building', ex)
